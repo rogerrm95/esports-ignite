98e0d1a07a142070524438d8175a47fec7b36555
rogerrm95 <rogerrm@live.com>
2022-09-13 19:49:53 -0300

    add: Stage 1/6

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+// const express = require('express) - Padrão //
+import express from 'express' // ECMAScript Modules //
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+    return response.json([
+        {id: 1, name: 'Anuncio 1'},
+        {id: 2, name: 'Anuncio 2'},
+        {id: 3, name: 'Anuncio 3'},
+        {id: 4, name: 'Anuncio 4'},
+    ])
+})
+
+// localhost:3333 //
+app.listen(3333)
