a4578f4e572c9f23b1c1d922dc2dd54909f22381
rogerrm95 <rogerrm@live.com>
2022-10-08 22:18:47 -0300

    feats: #1 Create Ad (Banner) and forms (Modal)

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,18 +1,24 @@
 import { useEffect, useState, useContext } from 'react';
-import { Image, FlatList, View } from 'react-native';
+import { Image, FlatList, View, TouchableOpacity, Text, TextInput, ScrollView, Modal } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
+import { UserContext } from '../../contexts/UserContext';
+import { Picker } from '@react-native-picker/picker';
+import InputTime from '@react-native-community/datetimepicker'
 import api from '../../services/axios';
-// LOGO //
+// Icons & Images //
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import Entypo from '@expo/vector-icons/Entypo'
+import { GameController } from 'phosphor-react-native';
 import logoIMG from '../../assets/logo-nlw-esports.png'
 // Components //
 import { Background } from '../../components/Background';
 import { GameCard, GameCardData } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
+import { Profile } from '../../components/Profile';
 // Styles //
 import { styles } from './styles';
-import { UserContext } from '../../contexts/UserContext';
-import { Profile } from '../../components/Profile';
+import { THEME } from '../../theme';
 
 interface Game {
     id: string,
@@ -28,6 +34,7 @@ export function Home() {
     const { data } = useContext(UserContext)
 
     const [games, setGames] = useState<Game[]>([])
+    const [isModalOpen, setIsModalOpen] = useState(false)
 
     useEffect(() => {
         async function load() {
@@ -45,33 +52,152 @@ export function Home() {
         })
     }
 
+    function handleToggleModal() {
+        setIsModalOpen(!isModalOpen)
+    }
+
     return (
         <Background>
-            <SafeAreaView style={styles.container}>
-                <View style={styles.header}>
-                    <Profile data={data} />
-                </View>
+            <SafeAreaView>
+                <ScrollView>
+                    <View style={styles.container}>
+                        {/* CABEÇALHO */}
+                        <View style={styles.header}>
+                            <Profile data={data} />
+                        </View>
 
+                        {/* LOGO */}
+                        <Image source={logoIMG} style={styles.logo} />
 
-                <Image source={logoIMG} style={styles.logo} />
+                        {/* TÍTULO */}
+                        <Heading
+                            title="Encontre seu duo!"
+                            subtitle="Selecione o game que deseja jogar..." />
 
-                <Heading
-                    title="Encontre seu duo!"
-                    subtitle="Selecione o game que deseja jogar..." />
+                        {/* LISTA DOS GAMES */}
+                        <FlatList
+                            data={games}
+                            keyExtractor={(item) => item.id}
+                            horizontal
+                            showsHorizontalScrollIndicator={false}
+                            renderItem={({ item }) => (
+                                <GameCard data={item} onPress={() => handleOpenGame(item)} />
+                            )}
+                            contentContainerStyle={styles.contentList}
+                        />
 
-                {/* LISTA DOS GAMES */}
-                <FlatList
-                    data={games}
-                    keyExtractor={(item) => item.id}
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                    renderItem={({ item }) => (
-                        <GameCard data={item} onPress={() => handleOpenGame(item)} />
-                    )}
-                    contentContainerStyle={styles.contentList}
-                />
+                        {/* CRIAR ANÚNCIO - BANNER */}
+                        <View style={styles.adBanner}>
+                            <View style={styles.bannerContent}>
+                                <View>
+                                    <Text style={styles.bannerTitle}>Não encontrou seu Duo ?</Text>
+                                    <Text style={styles.bannerLegend}>Publique um anúncio e encontre novos players!</Text>
+                                </View>
+
+                                <Entypo name='game-controller' size={24} color={THEME.COLORS.CAPTION_500} />
+                            </View>
+
+                            <TouchableOpacity style={styles.buttonNewAd} onPress={handleToggleModal}>
+                                <FontAwesome name='search-plus' color={THEME.COLORS.TEXT} size={16} />
+                                <Text style={styles.buttonNewAdLabel}>Novo anúnico</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        {/* CRIAR ANÚNCIO - MODAL */}
+                        <Modal visible={isModalOpen} transparent animationType="slide" onRequestClose={() => setIsModalOpen(!isModalOpen)}>
+                            <View style={styles.containerModal}>
+                                <View style={styles.contentModal}>
+                                    {/* TÍTULO */}
+                                    <Text style={styles.title}>
+                                        Publique um anúncio
+                                    </Text>
+
+                                    {/* FORMULÁRIO */}
+                                    <View style={styles.form}>
+                                        {/* JOGO - SELECT */}
+                                        <View>
+                                            <Text style={styles.label}>Qual o game ?</Text>
+
+                                            <View style={styles.select}>
+                                                <Picker style={{ color: THEME.COLORS.TEXT }} dropdownIconColor={THEME.COLORS.TEXT}>
+                                                    <Picker.Item label='Fifa 2023' value='Fifa 2023' />
+                                                    <Picker.Item label='Fortinite' value='Fortinite' />
+                                                </Picker>
+                                            </View>
+                                        </View>
+
+                                        {/* NOME - INPUT */}
+                                        <View>
+                                            <Text style={styles.label}>Seu nome (ou nickname)</Text>
+                                            <TextInput style={styles.input}
+                                                placeholder='Como te chamam dentro do game?'
+                                                placeholderTextColor={THEME.COLORS.CAPTION_500} />
+                                        </View>
+
+                                        {/* TEMPO DE JOGO - INPUT */}
+                                        <View>
+                                            <Text style={styles.label}>Joga há quantos anos ?</Text>
+                                            <TextInput style={styles.input}
+                                                contextMenuHidden
+                                                keyboardType='numeric'
+                                                placeholder='Tudo bem ser ZERO'
+                                                placeholderTextColor={THEME.COLORS.CAPTION_500} />
+                                        </View>
+
+                                        {/* DISCORD - INPUT */}
+                                        <View>
+                                            <Text style={styles.label}>Discord</Text>
+                                            <TextInput style={styles.input}
+                                                placeholder='Usuario#0000'
+                                                placeholderTextColor={THEME.COLORS.CAPTION_500} />
+                                        </View>
+
+                                        {/* DIAS DA SEMANA - INPUT */}
+                                        <View>
+                                            <Text style={styles.label}>Discord</Text>
+                                            <TextInput style={styles.input}
+                                                placeholder='Usuario#0000'
+                                                placeholderTextColor={THEME.COLORS.CAPTION_500} />
+                                        </View>
+
+                                        {/* HORÁRIOS - INPUTS */}
+                                        <View>
+                                            <Text style={styles.label}>Qual horário do dia?</Text>
+
+                                            {/* Continuar
+                                            <View style={styles.inputGroup}>
+                                                <InputTime style={[styles.input, styles.inputDate]}
+                                                    keyboardType='numeric'
+                                                    textContentType='oneTimeCode'
+                                                    placeholder='De'
+                                                    placeholderTextColor={THEME.COLORS.CAPTION_500} />
+
+                                                <TextInput style={[styles.input, styles.inputDate]}
+                                                    placeholder='Até'
+                                                    placeholderTextColor={THEME.COLORS.CAPTION_500} />
+                                            </View> 
+                                            */}
+                                        </View>
+
+                                    </View>
+
+                                    {/* BOTÕES */}
+                                    <View>
+                                        <TouchableOpacity>
+                                            <Text>Cancelar</Text>
+                                        </TouchableOpacity>
+
+                                        <TouchableOpacity>
+                                            <GameController size={14} color={THEME.COLORS.TEXT} />
+                                            <Text>Encontrar Duo</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            </View>
+                        </Modal>
+                    </View>
+                </ScrollView>
             </SafeAreaView>
-        </Background>
-
+        </Background >
     );
 }
--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 24,
   },
 
   header: {
@@ -22,5 +24,116 @@ export const styles = StyleSheet.create({
   contentList: {
     paddingLeft: 32,
     paddingRight: 64
-  }
+  },
+
+  // CRIAR ANÚNCIO - BANNER // 
+  adBanner: {
+    backgroundColor: THEME.COLORS.SHAPE,
+    borderRadius: 6,
+    marginTop: 24,
+    width: '100%',
+    maxWidth: 311,
+    gap: 24,
+    overflow: 'hidden',
+  },
+
+  bannerContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 12,
+    marginBottom: 24
+  },
+
+  bannerTitle: {
+    color: THEME.COLORS.TEXT,
+    fontSize: THEME.FONT_SIZE.MD,
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+    marginBottom: 4
+  },
+
+  bannerLegend: {
+    color: THEME.COLORS.CAPTION_400,
+    fontSize: THEME.FONT_SIZE.XXS
+  },
+
+  buttonNewAd: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 32,
+    backgroundColor: THEME.COLORS.PRIMARY,
+  },
+
+  buttonNewAdLabel: {
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+    fontSize: THEME.FONT_SIZE.XS,
+    marginLeft: 8
+  },
+
+  // CRIAR ANÚNCIO - MODAL //
+  containerModal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  contentModal: {
+    backgroundColor: THEME.COLORS.SHAPE,
+    padding: 32,
+    width: "100%",
+    height: '100%',
+    marginTop: 20,
+  },
+
+  title: {
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.BLACK,
+    fontSize: THEME.FONT_SIZE.LG,
+    marginBottom: 32
+  },
+
+  form: {
+    width: '100%'
+  },
+
+  label: {
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+    marginBottom: 8
+  },
+
+  select: {
+    height: 50,
+    backgroundColor: THEME.COLORS.BACKGROUND_900,
+    marginBottom: 12,
+    paddingLeft: 8,
+    borderRadius: 8
+  },
+
+  input: {
+    height: 50,
+    paddingHorizontal: 16,
+    marginBottom: 12,
+    backgroundColor: THEME.COLORS.BACKGROUND_900,
+    color: THEME.COLORS.TEXT,
+    borderRadius: 8
+  },
+
+  inputGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%'
+  },
+
+  inputDate: {
+    width: 150
+  },
+
+  buttons: {},
+
+  cancelButton: {},
+
+  submitButton: {},
 });
