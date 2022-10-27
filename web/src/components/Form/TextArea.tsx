ed0a3d3c7b3b03714b13ea87f19ea983d44510e8
rogerrm95 <rogerrm@live.com>
2022-10-26 22:12:03 -0300

    add: TextArea Component - applying Compound Component Pattern

---
+++ web/src/components/Form/TextArea.tsx
@@ -0,0 +1,46 @@
+import { HTMLAttributes, ReactNode } from "react"
+
+interface TextAreaRootProps {
+    children: ReactNode
+}
+
+function TextAreaRoot({ children }: TextAreaRootProps) {
+    return (
+        <div className="flex flex-col gap-4">
+            {children}
+        </div>
+    )
+}
+
+interface TextAreaFieldProps extends HTMLAttributes<HTMLTextAreaElement> { }
+
+function TextAreaField({ ...rest }: TextAreaFieldProps) {
+    return (
+        <textarea className="bg-zinc-900 text-white placeholder:text-zinc-500 p-3 leading-relaxed h-[160px] rounded outline-none"  {...rest} />
+    )
+}
+
+interface TextAreaLabelProps extends HTMLAttributes<HTMLLabelElement> {
+    children: ReactNode
+}
+
+function TextAreaLabel({ children, ...rest }: TextAreaLabelProps) {
+    return (
+        <label className="text-zinc-300 font-semibold" {...rest}>
+            {
+                children
+            }
+        </label>
+    )
+}
+
+TextAreaRoot.displayName = "TextArea.Root"
+TextAreaField.displayName = "TextArea.Field"
+TextAreaLabel.displayName = "TextArea.Label"
+
+
+export const TextArea = {
+    Root: TextAreaRoot,
+    Field: TextAreaField,
+    Label: TextAreaLabel
+}
