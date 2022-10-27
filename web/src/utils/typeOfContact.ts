317ce702c216015ab8b9f1457b480c68579b8735
rogerrm95 <rogerrm@live.com>
2022-10-26 22:13:23 -0300

    utils: type of contact list

---
+++ web/src/utils/typeOfContact.ts
@@ -0,0 +1,18 @@
+export const typeOfContact = [
+    {
+        key: 'question',
+        value: 'Dúvidas',
+    },
+    {
+        key: 'suggestion',
+        value: 'Sugestão',
+    },
+    {
+        key: 'complaint',
+        value: 'Reclamação',
+    },
+    {
+        key: 'support',
+        value: 'Suporte Técnico',
+    }
+]
