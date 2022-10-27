f26dab08cca97905fbe1e9b5b875f7da507ddf1e
rogerrm95 <rogerrm@live.com>
2022-10-26 22:13:58 -0300

    changes: refactoring component

--- web/src/components/Form/Select.tsx
+++ web/src/components/Form/Select.tsx
@@ -1,28 +1,30 @@
 import * as SelectUI from '@radix-ui/react-select'
 // Icon //
 import { CaretDown, Check, GameController } from 'phosphor-react'
+import { ReactNode } from 'react'
 
-type Game = {
-    id: string,
-    title: string,
+type Options = {
+    key: string,
+    value: string,
 }
 
 interface SelectProps extends SelectUI.SelectTriggerProps {
-    options: Game[],
+    options: Options[],
     label: string,
     placeholder: string,
     name: string,
     onSelectedChange: (option: string) => void
+    icon?: ReactNode
 }
 
-export function Select({ options, label, placeholder, name, onSelectedChange, ...rest }: SelectProps) {
+export function Select({ options, label, placeholder, icon = undefined, name, onSelectedChange, ...rest }: SelectProps) {
     return (
         <SelectUI.Root onValueChange={(option) => onSelectedChange(option)}>
             <SelectUI.Trigger
                 aria-label={label}
                 className='inline-flex items-center justify-between bg-zinc-900 py-3 px-4 rounded text-zinc-500 text-sm'
                 {...rest}>
-                <SelectUI.Value placeholder={placeholder}/>
+                <SelectUI.Value placeholder={placeholder} />
                 <SelectUI.Icon>
                     <CaretDown size={16} />
                 </SelectUI.Icon>
@@ -36,18 +38,27 @@ export function Select({ options, label, placeholder, name, onSelectedChange, ..
                         <SelectUI.Group>
 
                             <SelectUI.Label className='text-white font-bold flex items-center justify-center gap-2'>
-                                <GameController size={20} />
+                                {
+                                    icon && (
+                                        <SelectUI.Icon>
+                                            {
+                                                icon
+                                            }
+                                        </SelectUI.Icon>
+                                    )
+                                }
+                                
                                 {label}
                             </SelectUI.Label>
 
                             {
                                 options.map((option) => (
                                     <SelectUI.Item
-                                        key={option.id}
-                                        value={option.id}
+                                        key={option.key}
+                                        value={option.key}
                                         className='mt-2 flex items-center gap-1 h-8 p-2 hover:bg-violet-500 text-white text-xs rounded cursor-pointer'>
 
-                                        <SelectUI.ItemText>{option.title}</SelectUI.ItemText>
+                                        <SelectUI.ItemText>{option.value}</SelectUI.ItemText>
 
                                         <SelectUI.ItemIndicator>
                                             <Check size={16} />
